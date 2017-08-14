f534dfa421e98ea8326818b51b650c38fa1e5164
Zack Tylor <taylor.zackory@gmail.com>
2017-08-14 08:25:25 -0500

    generate seeds for demo.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.destroy_all
+Item.destroy_all
+
+
+10.times do
+  Todo.create(title: "#{Faker::TheFreshPrinceOfBelAir.quote}",
+              created_by: "#{Faker::TheFreshPrinceOfBelAir.character}"
+  )
+end
+
+20.times do
+  Item.create(name: "Fast travel to #{Faker::Zelda.location}.",
+              done: false,
+              todo: Todo.order("RANDOM()").first
+  )
+end
