36f5e2b440df58de0db363d157f101bd07d33ea2
Zack Tylor <taylor.zackory@gmail.com>
2017-08-13 22:31:06 -0500

    add tests for Todo and Item models.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # item record belongs to a single todo record
+  it { should belong_to(:todo) }
+
+  # Coulmn must have name
+  it { should validate_presence_of(:name) }
 end
